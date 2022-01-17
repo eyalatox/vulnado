636c25e3dc976e8ff3f151c801c7a474b0ea22b7
Eyal Paz <eyal@ox.security>
2022-01-17 11:54:04 +0000

    add projects and mapping

--- README.md
+++ README.md
@@ -1,23 +1,27 @@
 This project contain vulnerable projects to play and test
 
-|  Repo | Language | SAST | SCA | Secrets |
-|---|---|---|---|---|
-| dvws-node              | JavaScript | |
-| yooz-vulnerable-webapp | PHP        |
-| railsgoat              | Ruby       |
-| dvpwa                  | Python     |
-| DSVW                   | Python     |
-| dvja                   | Java       |
-| JavaVulnerableLab      | Java       |
-| CORS-vulnerable-Lab    | PHP        |
-| lets-be-bad-guys       | JavaScript |
-| vulpy                  | Python     |
-| vulnerable-app         | JavaScript |
-| dvcsharp-api           | C#         |
-| netanya-cyber-2021b    | Python     |
-| DVWA                   | PHP        |
-| govwa                  | Go         |
-| django.nV              | Python     |
-| VulnerableApp          | Java       |
-| dvna                   | JavaScript |
-| ASPVulnerableLab       | C#         |
+|  Repo | Language | SAST | SCA | Secrets | IaC |
+|---|---|---|---|---|---|
+| dvws-node              | JavaScript | X |   | X |   |
+| yooz-vulnerable-webapp | PHP        | X | X | X |   |
+| railsgoat              | Ruby       | X |   |   | X |
+| dvpwa                  | Python     | X |   |   |   |
+| DSVW                   | Python     | X |   |   |   |
+| dvja                   | Java       | X |   | X |   |
+| JavaVulnerableLab      | Java       | X |   |   |   |
+| CORS-vulnerable-Lab    | PHP        | X |   |   |   |
+| lets-be-bad-guys       | JavaScript | X |   |   |   |
+| vulpy                  | Python     | X |   | X |   |
+| vulnerable-app         | JavaScript | X |   |   |   |
+| dvcsharp-api           | C#         | X |   | X |   |
+| netanya-cyber-2021b    | Python     | X |   | X |   |
+| DVWA                   | PHP        | X |   |   |   |
+| govwa                  | Go         | X |   |   |   |
+| django.nV              | Python     | X |   |   |   |
+| VulnerableApp          | Java       | X |   | X |   |
+| dvna                   | JavaScript | X |   |   |   |
+| ASPVulnerableLab       | C#         | X |   |   |   |
+| WebGoat.NET            | C#         | X |   |   |   |
+| dvta                   | C#         | X |   |   |   |
+| VulnerableApp-facade   | TypeScript | X |   |   |   |
+| iam-vulnerable         | Terraform  |   |   |   | X |
---
+++ VulnerableApp-facade/facade-app/src/Components/Content.tsx
@@ -0,0 +1,157 @@
+import React from "react";
+import { LevelInformation } from "../interface/State";
+import { Panel as RSuitePanel } from "rsuite";
+import {
+  appendStaticResourcesToDocument,
+  getResource,
+  manipulateDOM,
+} from "../Utilities/Utils";
+import { VulnerabilityDefinitionResponse } from "../interface/GeneralContracts";
+import { HomePage } from "./HomePage";
+import { AboutUs } from "./AboutUs";
+import { Props } from "../interface/Props";
+
+export class Content extends React.Component<Props> {
+  selectedLevel?: LevelInformation;
+
+  componentDidUpdate(prevProps: Props) {
+    if (
+      prevProps.globalState.activeLevel !==
+        this.props.globalState.activeLevel ||
+      prevProps.globalState.activeVulnerability !==
+        this.props.globalState.activeVulnerability ||
+      prevProps.globalState.activeApplication !==
+        this.props.globalState.activeApplication
+    ) {
+      const {
+        activeApplication,
+        applicationData,
+        activeVulnerability,
+        activeLevel,
+      } = this.props.globalState;
+      const selectedApplicationState = applicationData?.find(
+        (applicationState) =>
+          applicationState.applicationName === activeApplication
+      );
+      if (selectedApplicationState) {
+        const selectedVulnerability =
+          selectedApplicationState.vulnerabilityDefinitions.find(
+            (vulnerabilityDefinition) =>
+              vulnerabilityDefinition.id === activeVulnerability
+          );
+        this.setState({ description: selectedVulnerability?.description });
+        if (selectedVulnerability) {
+          manipulateDOM(
+            "__vuln_description__",
+            selectedVulnerability.description
+          );
+          const selectedLevel = selectedVulnerability.levels.find(
+            (level) => level.levelIdentifier === activeLevel
+          );
+          if (selectedLevel) {
+            this.selectedLevel = selectedLevel;
+            getResource(
+              selectedLevel.resourceInformation.htmlResource.uri,
+              this._setLocalState.bind(this),
+              false
+            );
+          }
+        }
+      }
+    }
+  }
+
+  _setLocalState(
+    vulnerabilityDefinitionResponse: VulnerabilityDefinitionResponse
+  ) {
+    if (vulnerabilityDefinitionResponse.data) {
+      manipulateDOM("__content__", vulnerabilityDefinitionResponse.data);
+      if (this.selectedLevel) {
+        appendStaticResourcesToDocument(this.selectedLevel);
+      }
+    }
+  }
+
+  render() {
+    const {
+      activeVulnerability,
+      activeApplication,
+      activeLevel,
+      activateHomePage,
+      activateAboutUsPage,
+      showHints,
+    } = this.props.globalState;
+    const { setGlobalState } = this.props;
+    return (
+      <div className="VulnerableApp-Facade-Info">
+        {activateHomePage ? (
+          <HomePage></HomePage>
+        ) : activateAboutUsPage ? (
+          <AboutUs></AboutUs>
+        ) : (
+          <div>
+            {activeVulnerability ? (
+              <div>
+                <RSuitePanel
+                  header="Vulnerability Description"
+                  className="VulnerableApp-Facade-Content-Vulnerability-Description-Header"
+                  collapsible={true}
+                  defaultExpanded={false}
+                >
+                  <div className="VulnerableApp-Facade-Content">
+                    <div id="__vuln_description__" />
+                  </div>
+                </RSuitePanel>
+              </div>
+            ) : (
+              <div />
+            )}
+            {activeVulnerability ? (
+              <div>
+                <RSuitePanel
+                  header="Practice Vulnerability"
+                  className="VulnerableApp-Facade-Content-Practice-Vulnerability-Header"
+                >
+                  <div className="VulnerableApp-Facade-Content">
+                    <div id="__content__" />
+                  </div>
+                </RSuitePanel>
+                {this.selectedLevel &&
+                this.selectedLevel.hints &&
+                this.selectedLevel.hints.length > 0 ? (
+                  <RSuitePanel
+                    header="Hints"
+                    className="VulnerableApp-Facade-Content-Hint-Content"
+                    collapsible={true}
+                    defaultExpanded={false}
+                    expanded={showHints}
+                    onSelect={() =>
+                      setGlobalState({
+                        activeApplication: activeApplication,
+                        activeVulnerability: activeVulnerability,
+                        activeLevel: activeLevel,
+                        activateHomePage: false,
+                        activateAboutUsPage: false,
+                        showHints: !showHints,
+                      })
+                    }
+                  >
+                    <ol>
+                      {this.selectedLevel.hints.map((hint) => {
+                        return <li>{hint.description}</li>;
+                      })}
+                    </ol>
+                  </RSuitePanel>
+                ) : (
+                  <div />
+                )}
+              </div>
+            ) : (
+              <div></div>
+            )}
+          </div>
+        )}
+      </div>
+    );
+  }
+}
